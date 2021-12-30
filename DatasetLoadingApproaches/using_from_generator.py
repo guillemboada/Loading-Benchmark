5950c13e5823d5778ef503458b49761a0df2d155
Guillem Boada <guillemboada@hotmail.com>
2021-12-30 19:06:19 +0100

    Add approach with manual caching (preload data)

--- DatasetLoadingApproaches/using_from_generator.py
+++ DatasetLoadingApproaches/using_from_generator.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+from tqdm import tqdm
 import cv2
 import random
 import numpy as np
@@ -28,16 +29,26 @@ def read_mask(path):
 
     return x
 
-def load_samples(path_list, type):
-    samples = []
-    for path in path_list:
-        if type=="images":
-            sample = read_image(path)
-        if type=="masks":
-            sample = read_mask(path)
+def load_sample(path, type):
+    if type=="images":
+        sample = read_image(path)
+    if type=="masks":
+        sample = read_mask(path)
 
-        sample = cv2.resize(sample, (WIDTH, HEIGHT))
-        samples.append(sample)
+    sample = cv2.resize(sample, (WIDTH, HEIGHT))
+
+    return sample
+
+def load_samples(path_list, type, verbose=0):
+    samples = []
+    if verbose>0:
+        for path in tqdm(path_list):
+            sample = load_sample(path, type)
+            samples.append(sample)
+    else:
+        for path in path_list:
+            sample = load_sample(path, type)
+            samples.append(sample)
 
     return samples
 
@@ -67,10 +78,42 @@ def generator_from_paths(x_path, y_path, batch_size):
             x_path = list(x_path)
             y_path = list(y_path)
 
-def define_create_generator(path, batch_size):
+def generator_from_data(images, masks, batch_size):
+    size = len(images)
+    i = 0
+    while True:
+        real_batch_size = min(batch_size, size - i)
+        x_data = images[i:i+real_batch_size]
+        y_label = masks[i:i+real_batch_size]
+        shape = x_data[0].shape
+        batch_x = np.zeros((real_batch_size, shape[0], shape[1], shape[2])).astype('float32')
+        batch_y = np.zeros((real_batch_size, shape[0], shape[1])).astype('float32')
+
+        for k in range(i, i + real_batch_size):
+            batch_x[k-i] = x_data[k-i]
+            batch_y[k-i] = y_label[k-i]
+
+        yield batch_x, batch_y
+        i += real_batch_size
+        if i >= size:
+            i = 0
+            # Shuffle data
+            c = list(zip(images, masks))
+            random.shuffle(c)
+            images, masks = zip(*c)
+            images = list(images)
+            masks = list(masks)
+
+def define_create_generator_from_paths(path, batch_size):
     def create_generator():
         images_paths, masks_paths = get_data_paths(path)
         for image, mask in generator_from_paths(images_paths, masks_paths, batch_size):
+            yield image, mask
+    return create_generator
+
+def define_create_generator_from_data(images, masks, batch_size):
+    def create_generator():
+        for image, mask in generator_from_data(images, masks, batch_size):
             yield image, mask
     return create_generator
 
@@ -80,13 +123,25 @@ def get_data_paths(path):
 
     return images_paths, masks_paths
 
-def get_generator(path, batch_size):
-    create_generator = define_create_generator(path, batch_size)
+def get_generator_from_paths(path, batch_size):
+    create_generator = define_create_generator_from_paths(path, batch_size)
     generator = create_generator()
     return generator
 
-def get_dataset(path, batch_size):
-    dataset = tf.data.Dataset.from_generator(define_create_generator(path, batch_size), output_types=(tf.float32, tf.float32))
+def get_generator_from_data(images, masks, batch_size):
+    create_generator = define_create_generator_from_data(images, masks, batch_size)
+    generator = create_generator()
+    return generator
+
+def get_dataset_from_paths(path, batch_size):
+    dataset = tf.data.Dataset.from_generator(define_create_generator_from_paths(path, batch_size), output_types=(tf.float32, tf.float32))
+    dataset = dataset.repeat()
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    
+    return dataset
+
+def get_dataset_from_data(images, masks, batch_size):
+    dataset = tf.data.Dataset.from_generator(define_create_generator_from_data(images, masks, batch_size), output_types=(tf.float32, tf.float32))
     dataset = dataset.repeat()
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     
