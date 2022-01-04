8139c43688c092d52d3b63258036a08995b80934
Guillem Boada <guillemboada@hotmail.com>
2022-01-04 11:36:25 +0100

    Add approach of semipreloaded data (as uint)

--- DatasetLoadingApproaches/using_from_generator.py
+++ DatasetLoadingApproaches/using_from_generator.py
@@ -12,42 +12,50 @@ HEIGHT = int(config['DEFAULT']['height'])
 WIDTH = int(config['DEFAULT']['width'])
 
 
-def read_image(path):
+def get_data_paths(path):
+    images_paths = glob(os.path.join(path, "images/*"))
+    masks_paths = glob(os.path.join(path, "masks/*"))
+
+    return images_paths, masks_paths
+
+def read_image(path, preprocess=True):
     x = cv2.imread(path, cv2.IMREAD_COLOR)
     x = cv2.resize(x, (WIDTH, HEIGHT))
-    x = x / 255.0
-    x = x.astype(np.float32)
+    if preprocess:
+        x = x / 255.0
+        x = x.astype(np.float32)
 
     return x
 
-def read_mask(path):
+def read_mask(path, preprocess=True):
     x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     x = cv2.resize(x, (WIDTH, HEIGHT))
     x -= 1
     x = np.expand_dims(x, axis=-1)
-    x = x.astype(np.float32)
+    if preprocess:
+        x = x.astype(np.float32)
 
     return x
 
-def load_sample(path, type):
+def load_sample(path, type, preprocess=True):
     if type=="images":
-        sample = read_image(path)
+        sample = read_image(path, preprocess)
     if type=="masks":
-        sample = read_mask(path)
+        sample = read_mask(path, preprocess)
 
     sample = cv2.resize(sample, (WIDTH, HEIGHT))
 
     return sample
 
-def load_samples(path_list, type, verbose=0):
+def load_samples(path_list, type, preprocess=True, verbose=0):
     samples = []
     if verbose>0:
         for path in tqdm(path_list):
-            sample = load_sample(path, type)
+            sample = load_sample(path, type, preprocess)
             samples.append(sample)
     else:
         for path in path_list:
-            sample = load_sample(path, type)
+            sample = load_sample(path, type, preprocess)
             samples.append(sample)
 
     return samples
@@ -78,7 +86,7 @@ def generator_from_paths(x_path, y_path, batch_size):
             x_path = list(x_path)
             y_path = list(y_path)
 
-def generator_from_data(images, masks, batch_size):
+def generator_from_preloaded_data(images, masks, batch_size, semipreloaded=False):
     size = len(images)
     i = 0
     while True:
@@ -92,6 +100,11 @@ def generator_from_data(images, masks, batch_size):
         for k in range(i, i + real_batch_size):
             batch_x[k-i] = x_data[k-i]
             batch_y[k-i] = y_label[k-i]
+
+        if semipreloaded:        
+            # Map preloaded data
+            batch_x = batch_x.astype('float32') / 255.
+            batch_y = batch_y.astype('float32')
 
         yield batch_x, batch_y
         i += real_batch_size
@@ -111,25 +124,19 @@ def define_create_generator_from_paths(path, batch_size):
             yield image, mask
     return create_generator
 
-def define_create_generator_from_data(images, masks, batch_size):
+def define_create_generator_from_preloaded_data(images, masks, batch_size, semipreloaded=False):
     def create_generator():
-        for image, mask in generator_from_data(images, masks, batch_size):
+        for image, mask in generator_from_preloaded_data(images, masks, batch_size, semipreloaded):
             yield image, mask
     return create_generator
-
-def get_data_paths(path):
-    images_paths = glob(os.path.join(path, "images/*"))
-    masks_paths = glob(os.path.join(path, "masks/*"))
-
-    return images_paths, masks_paths
 
 def get_generator_from_paths(path, batch_size):
     create_generator = define_create_generator_from_paths(path, batch_size)
     generator = create_generator()
     return generator
 
-def get_generator_from_data(images, masks, batch_size):
-    create_generator = define_create_generator_from_data(images, masks, batch_size)
+def get_generator_from_preloaded_data(images, masks, batch_size, semipreloaded=False):
+    create_generator = define_create_generator_from_preloaded_data(images, masks, batch_size, semipreloaded)
     generator = create_generator()
     return generator
 
@@ -140,8 +147,8 @@ def get_dataset_from_paths(path, batch_size):
     
     return dataset
 
-def get_dataset_from_data(images, masks, batch_size):
-    dataset = tf.data.Dataset.from_generator(define_create_generator_from_data(images, masks, batch_size), output_types=(tf.float32, tf.float32))
+def get_dataset_from_preloaded_data(images, masks, batch_size, semipreloaded=False):
+    dataset = tf.data.Dataset.from_generator(define_create_generator_from_preloaded_data(images, masks, batch_size, semipreloaded), output_types=(tf.float32, tf.float32))
     dataset = dataset.repeat()
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     
